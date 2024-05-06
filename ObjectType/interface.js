298372b44f4c36d92be33edf50b802817a9bcdc3
Mitesh <miteshsonagra5576@gmail.com>
2024-05-06 17:38:16 +0530

    js file

--- ObjectType/interface.js
+++ ObjectType/interface.js
@@ -15,3 +15,35 @@ console.log(filteredUser([{
         name: "Jay",
         age: 17
     }]));
+const invoice = {
+    name: "HP",
+    price: 20000,
+    qty: 5,
+    location: "22, apc circle",
+};
+const invoice1 = {
+    name: "HPC",
+    price: 2000,
+    qty: 2,
+    location: "22, apc circle",
+};
+const invoice2 = {
+    name: "HPPP",
+    price: 200000,
+    qty: 3
+};
+const invoice3 = {
+    name: "HPP",
+    price: 200002,
+    qty: 6
+};
+const invoices = [invoice, invoice1, invoice2, invoice3];
+invoices.forEach(inv => {
+    if (inv.price <= 20000) {
+        console.log(inv.name, "This laptop are under budget");
+    }
+});
+// OR
+// if (invoices.some(inv => inv.name === "HP")) {
+//     console.log("Hello");
+// }
---
+++ TypeScript_Function/functionOverloading.js
@@ -0,0 +1,7 @@
+"use strict";
+function add1(a, b) {
+    return a + b;
+}
+console.log(add1(10, 30));
+// console.log(add1(10, 30), add1("Mit", "esh"))
+// console.log(add1("Mit", "esh"))
